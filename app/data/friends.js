9ea35b17bc5b36eb659c42c62fc3ae825788b73a
Smith <Patricia_Smith@steris.com>
2018-01-29 20:40:50 -0500

    pushing friend-finder hw

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+  {
+    name: "Herbert",
+    photo: "/app/public/img/herbert.png",
+    scores: [
+      "2",
+      "5",
+      "4",
+      "1",
+      "5",
+      "3",
+      "2",
+      "5",
+      "4",
+      "2"
+    ]
+  },
+  {
+    name: "Jill",
+    photo: "/app/public/img/herbert.png",
+    scores: [
+      "3",
+      "5",
+      "4",
+      "1",
+      "3",
+      "1",
+      "2",
+      "1",
+      "3",
+      "3"
+    ]
+  },
+  {
+    name: "Slash",
+    photo: "/app/public/img/slash.png",
+    scores: [
+      "5",
+      "2",
+      "4",
+      "2",
+      "4",
+      "2",
+      "3",
+      "2",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Dexter",
+    photo: "/app/public/img/dexter.png",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "2",
+      "1",
+      "1",
+      "2",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Julio",
+    photo: "/app/public/img/Julio.png",
+    scores: [
+      "5",
+      "3",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "3",
+      "1",
+      "41"
+    ]
+  },
+  {
+    name: "Lil Eugene",
+    photo: "/app/public/img/lileugene.png",
+    scores: [
+      "5",
+      "4",
+      "2",
+      "3",
+      "2",
+      "3",
+      "3",
+      "2",
+      "4",
+      "2"
+    ]
+  }
+
+];
+
+// export the array
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,25 @@
+//dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+// Port
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// routes
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// listener
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
